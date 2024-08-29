06fc88eacc8bb1c15bb1a6e3c3ee39264e2352ef
Raksha-Kumari <kumariraksha1710@gmail.com>
2024-08-29 16:13:18 +0530

    Changes in Faq,Pagination,Comment

--- faq.js
+++ faq.js
@@ -1,18 +1,5 @@
 const accordionItems = document.querySelectorAll('.accordion-item');
-
-accordionItems.forEach(item => {
-    const title = item.querySelector('.accordion-title');
-    title.addEventListener('click', () => {
-        const openItem = document.querySelector('.accordion-item.open');
-
-        toggleItem(item);
-
-        if (openItem && openItem !== item) {
-            toggleItem(openItem);
-        }
-    });
-});
-
+// Function to toggle the accordion item
 const toggleItem = (item) => {
     const content = item.querySelector('.accordion-content');
     const icon = item.querySelector('.accordion-title i'); // Select the icon element
@@ -28,4 +15,23 @@ const toggleItem = (item) => {
         icon.classList.remove('fa-chevron-down'); // Change the icon to up arrow
         icon.classList.add('fa-chevron-up');
     }
-}
+};
+
+// Automatically open the first accordion item on page load
+window.addEventListener('load', () => {
+    const firstItem = accordionItems[0];
+    toggleItem(firstItem);
+});
+
+accordionItems.forEach(item => {
+    const title = item.querySelector('.accordion-title');
+    title.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-item.open');
+
+        toggleItem(item);
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem);
+        }
+    });
+});
